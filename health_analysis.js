d0f933dff30a46373e3547fc94f0eca4fd0d7459
pgordon-moric <pgordon@moric.org>
2026-07-17 16:33:15 -0400

    began building the js file

---
+++ health_analysis.js
@@ -0,0 +1,5 @@
+const addPatientButton = document.getElementById("addPatient");
+const report = document.getElementById("report");
+const btnSearch = document.getElementById("btnSearch");
+const patients = [];
+
